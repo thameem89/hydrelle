233592ba3164aead355c2ad4975b65ccdff4c898
Thameem <thameem@Thameems-MacBook-Air.local>
2026-05-06 22:07:18 +0400

    Fix bug: added missing useState and useEffect imports in product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { getProductById } from '@/lib/products';
 import Navbar from '@/components/Navbar';
